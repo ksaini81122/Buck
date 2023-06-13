aec3e2826505553d308d193254048776e6239d52
ksaini81122 <134952726+ksaini81122@users.noreply.github.com>
2023-06-13 10:42:18 +0530

    __init__.py

--- buck/__init__.py
+++ buck/__init__.py
@@ -6,6 +6,6 @@ openai_key="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"
 
 api_id="1125689"
 api_hash="4772d1792ed194020a8fb06a91ffb8fa"
-bot_token="5963395579:AAHf6N1WOkXTxRzQGNmX2EEm368J3KHFBxM"
+bot_token="5963395579:AAEKDr_oAfHQvpFDpp7Zt_8uxz0dnCmTh6Y"
 
 bot=TelegramClient("Gattuu_bot",api_id,api_hash).start(bot_token=bot_token)
